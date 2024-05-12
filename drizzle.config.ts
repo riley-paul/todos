1f16439ca020c9b028999c73fbb8f1f4d6e64a9c
Riley Paul <rileypaul96@gmail.com>
2024-05-11 21:59:39 -0600

    update paths

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { type Config } from "drizzle-kit";
 
 export default {
-  schema: "./db/schema.ts",
+  schema: "./src/api/db/schema.ts",
   driver: "turso",
   dialect: "sqlite",
   dbCredentials: {
