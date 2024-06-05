c9328d7f71766cc902bfcbc50dcfa5bf693259db
Riley Paul <rileypaul96@gmail.com>
2024-06-05 07:33:30 -0700

    remove locals types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,2 @@
 /// <reference types="astro/client" />
-declare namespace App {
-  interface Locals {
-    session: import("lucia").Session | null;
-    user: import("lucia").User | null;
-  }
-}
+declare namespace App {}
