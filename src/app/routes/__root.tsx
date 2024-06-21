6c5ddc7d016fc6db8d7e9469d210ffbe2b8dfa05
Riley Paul <rileypaul96@gmail.com>
2024-06-21 13:38:03 -0600

    get app rendering at root

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,17 +1,26 @@
+import ListSwitcher from "@/components/list-switcher";
+import UserAvatar from "@/components/user-avatar";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { CircleCheckBig } from "lucide-react";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="flex gap-2 p-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
+      <header className="sticky top-0 z-50 h-16 border-b bg-background">
+        <div className="container2 flex h-full items-center justify-between">
+          <div className="flex items-end gap-2">
+            <Link to="/">
+              <div className="flex items-center gap-2">
+                <CircleCheckBig size="1.5rem" className="text-primary" />
+                <div className="text-2xl font-bold">Todos</div>
+              </div>
+            </Link>
+            <ListSwitcher />
+          </div>
+          <UserAvatar user={null} />
+        </div>
+      </header>
+      <slot />
       <Outlet />
     </>
   ),
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,5 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import Adder from "@/components/adder";
+import TodoList from "@/components/todo-list";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-  component: () => <div>Hello /!</div>
-})
+export const Route = createFileRoute("/")({
+  component: () => (
+    <main className="container2 flex flex-col items-center gap-4 pt-6">
+      <article className="flex w-full flex-col gap-4">
+        <Adder />
+        <TodoList />
+      </article>
+    </main>
+  ),
+});
