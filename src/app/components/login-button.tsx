dd02abd7176fa8ff9b2bed58676d3a2fe1412ebc
Riley Paul <rileypaul96@gmail.com>
2024-07-05 14:57:59 -0600

    tweak dark mode

--- src/app/components/login-button.tsx
+++ src/app/components/login-button.tsx
@@ -13,13 +13,15 @@ const authProviders: Record<
     name: "GitHub",
     url: "/api/auth/login/github",
     icon: <FaGithub className="mr-2 h-5 w-5" />,
-    className: "bg-gray-900 text-white hover:bg-gray-800 border border-gray-700",
+    className:
+      "bg-gray-900 text-white hover:bg-gray-800 border border-gray-700 shadow",
   },
   google: {
     name: "Google",
     url: "/api/auth/login/google",
     icon: <FcGoogle className="mr-2 h-5 w-5" />,
-    className: "bg-white text-black hover:bg-gray-100",
+    className:
+      "bg-white text-black hover:bg-gray-100 shadow border border-gray-300",
   },
 };
 
