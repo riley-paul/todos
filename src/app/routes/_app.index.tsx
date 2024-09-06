c99da1b744a43dbdcaddb286c9fe8dbaa6f77185
Riley Paul <rileypaul96@gmail.com>
2024-09-06 15:00:54 -0600

    update document title

--- src/app/routes/_app.index.tsx
+++ src/app/routes/_app.index.tsx
@@ -1,19 +1,27 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useSearch } from "@tanstack/react-router";
 import Adder from "../components/adder";
 import TodoList from "../components/todo-list";
 import { z } from "zod";
 import HashtagList from "../components/hashtag-list";
-// import DeleteCompletedButton from "../components/delete-completed-button";
+import { useDocumentTitle } from "usehooks-ts";
 
-export const Route = createFileRoute("/_app/")({
-  validateSearch: (search) =>
-    z.object({ tag: z.string().optional() }).parse(search),
-  component: () => (
+const Component: React.FC = () => {
+  const { tag } = useSearch({ from: "/_app/" });
+  useDocumentTitle(`Todos${tag && tag !== "~" ? ` - ${tag}` : ""}`, {
+    preserveTitleOnUnmount: false,
+  });
+
+  return (
     <article className="flex w-full flex-col gap-4 pb-28 pt-6">
       <Adder />
       <HashtagList />
       <TodoList />
-      {/* <DeleteCompletedButton /> */}
     </article>
-  ),
+  );
+};
+
+export const Route = createFileRoute("/_app/")({
+  validateSearch: (search) =>
+    z.object({ tag: z.string().optional() }).parse(search),
+  component: Component,
 });
