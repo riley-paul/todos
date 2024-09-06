96f32369b82fa1b386548ddcc1810e9388287b8f
Riley Paul <rileypaul96@gmail.com>
2024-09-06 14:47:59 -0600

    update adder behaviour to prepopulate hashtag

--- src/app/components/adder.tsx
+++ src/app/components/adder.tsx
@@ -3,11 +3,25 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Loader2, Plus } from "lucide-react";
 import useMutations from "../hooks/use-mutations";
+import { useSearch } from "@tanstack/react-router";
+
+const isOnlyHashtag = (value: string) =>
+  value.startsWith("#") && value.split(" ").length === 1;
+
+const isEmptyString = (value: string) => value.trim() === "";
 
 export default function Adder(): ReturnType<React.FC> {
+  const { tag } = useSearch({ from: "/_app/" });
+
   const inputRef = React.useRef<HTMLInputElement>(null);
   const [value, setValue] = React.useState<string>("");
   const { createTodo } = useMutations();
+
+  React.useEffect(() => {
+    if (tag && (isOnlyHashtag(value) || isEmptyString(value))) {
+      setValue(tag === "~" ? "" : `#${tag}`);
+    }
+  }, [tag]);
 
   const create = () => {
     if (value) {
@@ -26,7 +40,11 @@ export default function Adder(): ReturnType<React.FC> {
         onChange={(e) => setValue(e.target.value)}
         className="text-md px-4"
         placeholder="Add a todo..."
-        onFocus={(e) => e.target.select()}
+        onFocus={(e) => {
+          if (!isOnlyHashtag(value)) {
+            e.target.select();
+          }
+        }}
         onKeyDown={(e) => {
           if (e.key === "Enter") {
             create();
