14eb410db50dff1e77ffb1e958c795baa6e7d509
Riley Paul <rileypaul96@gmail.com>
2024-09-20 14:23:25 -0600

    add db scripts

---
+++ db/scripts/restore-from-backup.ts
@@ -0,0 +1,28 @@
+import fs from "fs/promises";
+import { db } from "astro:db";
+import { allTables, type AnyTable } from "./_all-tables";
+
+async function restoreFromBackup({ table, name }: AnyTable) {
+  const file = await fs.readFile(`db/data/${name}.json`, "utf-8");
+  const data = JSON.parse(file);
+
+  try {
+    await db.insert(table).values(data);
+  } catch (error) {
+    console.error(`Error restoring ${name}`);
+    console.error(error);
+    console.log(data.slice(0, 5));
+  }
+}
+
+export default async function restoreAll() {
+  for (const table of allTables.toReversed()) {
+    await db.delete(table.table);
+    console.log(`Deleted ${table.name}`);
+  }
+
+  for (const table of allTables) {
+    console.log(`Restoring ${table.name}`);
+    await restoreFromBackup(table);
+  }
+}
