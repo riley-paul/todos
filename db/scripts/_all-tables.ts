8573aaca0e06ccc3bd1a5ef9e1423ec50b7b3acf
Riley Paul <rileypaul96@gmail.com>
2024-09-21 21:50:53 -0600

    add new table to scripts

--- db/scripts/_all-tables.ts
+++ db/scripts/_all-tables.ts
@@ -1,9 +1,10 @@
-import { User, UserSession, Todo } from "astro:db";
+import { User, UserSession, Todo, SharedTag } from "astro:db";
 
 export const allTables = [
   { table: User, name: "user" },
   { table: UserSession, name: "user_session" },
   { table: Todo, name: "todo" },
+  { table: SharedTag, name: "shared_tags" },
 ];
 
 export type AnyTable = (typeof allTables)[number];
