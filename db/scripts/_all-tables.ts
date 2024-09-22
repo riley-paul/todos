143a97513bf4e1f0ded8bb82b42ab524a779ec86
Riley Paul <rileypaul96@gmail.com>
2024-09-22 07:55:50 -0600

    remove extra table

--- db/scripts/_all-tables.ts
+++ db/scripts/_all-tables.ts
@@ -1,10 +1,9 @@
-import { User, UserSession, Todo, SharedTag } from "astro:db";
+import { User, UserSession, Todo } from "astro:db";
 
 export const allTables = [
   { table: User, name: "user" },
   { table: UserSession, name: "user_session" },
   { table: Todo, name: "todo" },
-  { table: SharedTag, name: "shared_tags" },
 ];
 
 export type AnyTable = (typeof allTables)[number];
