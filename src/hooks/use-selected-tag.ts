25341ce286f25cc410376b1691e40f34a2aa9cdb
Riley Paul <rileypaul96@gmail.com>
2024-10-01 17:22:36 -0600

    delete more files

--- src/hooks/use-selected-tag.ts
+++
@@ -1,7 +0,0 @@
-import { useSearchParams } from "react-router-dom";
-
-export default function useSelectedTag() {
-  const [searchParams] = useSearchParams();
-  const tag = searchParams.get("tag") ?? "";
-  return { tag };
-}
