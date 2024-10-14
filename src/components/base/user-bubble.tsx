6945697d7d9c28f2207783655d17511407a8ae72
Riley Paul <rileypaul96@gmail.com>
2024-10-14 07:41:18 -0600

    break out pending invites from header

--- src/components/base/user-bubble.tsx
+++ src/components/base/user-bubble.tsx
@@ -2,15 +2,24 @@ import type { UserSelect } from "@/lib/types";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+type Size = "sm" | "md" | "lg";
+
 type Props = {
   user: UserSelect;
+  size?: Size;
+};
+
+const sizeMap: Record<Size, string> = {
+  sm: "size-5",
+  md: "size-8",
+  lg: "size-12",
 };
 
 const UserBubble: React.FC<Props> = (props) => {
-  const { user } = props;
+  const { user, size = "sm" } = props;
 
   return (
-    <Avatar className="size-5">
+    <Avatar className={sizeMap[size]}>
       <AvatarImage src={user.avatarUrl ?? ""} alt={user.name} />
       <AvatarFallback>{user.name[0]}</AvatarFallback>
     </Avatar>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,9 @@
 import React from "react";
 import { cn } from "../lib/utils";
 import UserAvatar from "./user-avatar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Button } from "@/components/ui/button";
-import { useQuery } from "@tanstack/react-query";
-import { pendingSharesQueryOptions } from "@/lib/queries";
-import UserBubble from "./base/user-bubble";
-import useMutations from "@/hooks/use-mutations";
+import PendingInvites from "./pending-invites";
 
 const Header: React.FC = () => {
-  const pendingSharesQuery = useQuery(pendingSharesQueryOptions);
-  const numPendingShares = pendingSharesQuery.data?.length ?? 0;
-  const { acceptListShare, deleteListShare } = useMutations();
-
   return (
     <header
       className={cn(
@@ -29,64 +16,7 @@ const Header: React.FC = () => {
           <div className="font-bold">Todos</div>
         </div>
         <div className="flex items-center gap-4">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant="secondary"
-                size="icon"
-                className="relative rounded-full"
-              >
-                <i className="fa-solid fa-bell" />
-                {numPendingShares > 0 && (
-                  <div className="absolute -right-3 -top-1 rounded-full bg-primary px-2 py-0.5 text-xs">
-                    {numPendingShares}
-                  </div>
-                )}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent align="end" className="py-2">
-              <div className="grid divide-y">
-                {pendingSharesQuery.data?.map((share) => (
-                  <div className="flex items-center gap-3 py-2">
-                    <div className="h-full">
-                      <UserBubble user={share.invitedBy} />
-                    </div>
-                    <div className="grid gap-2">
-                      <span className="text-sm">
-                        <b>{share.invitedBy.name}</b> invited you to join{" "}
-                        <b>{share.list.name}</b>
-                      </span>
-                      <div className="grid grid-cols-2 gap-2">
-                        <Button
-                          size="sm"
-                          variant="secondary"
-                          onClick={() =>
-                            acceptListShare.mutate({ id: share.id })
-                          }
-                        >
-                          <span>Accept</span>
-                        </Button>
-                        <Button
-                          size="sm"
-                          variant="outline"
-                          onClick={() =>
-                            deleteListShare.mutate({ id: share.id })
-                          }
-                        >
-                          <span>Decline</span>
-                        </Button>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-                {pendingSharesQuery.data?.length === 0 && (
-                  <div className="flex min-h-12 items-center justify-center text-sm text-muted-foreground">
-                    No pending shares
-                  </div>
-                )}
-              </div>
-            </PopoverContent>
-          </Popover>
+          <PendingInvites />
           <UserAvatar />
         </div>
       </div>
