ed0d26e141241b544a24b18bcdbd0796b987e141
Riley Paul <rileypaul96@gmail.com>
2025-01-12 09:57:58 -0700

    add transition to bg colours

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -112,7 +112,7 @@ const Todo: React.FC<{ todo: TodoSelect }> = ({ todo }) => {
   return (
     <div
       ref={ref}
-      className="flex min-h-11 items-center gap-rx-2 rounded-3 px-rx-3 py-rx-1 hover:bg-gray-3"
+      className="flex min-h-11 items-center gap-rx-2 rounded-3 px-rx-3 py-rx-1 hover:bg-gray-3 transition-colors ease-out"
     >
       {editorOpen ? (
         <TodoForm
