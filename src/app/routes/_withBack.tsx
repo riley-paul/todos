4a51633b8fe6dcaf7825c256e965e03aeb6c55c6
Riley Paul <rileypaul96@gmail.com>
2025-01-28 07:48:53 -0700

    tweak back button

--- src/app/routes/_withBack.tsx
+++ src/app/routes/_withBack.tsx
@@ -1,16 +1,22 @@
 import { Button } from "@radix-ui/themes";
-import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
+import {
+  createFileRoute,
+  Link,
+  Outlet,
+  useParams,
+} from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_withBack")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { listId } = useParams({ strict: false });
   return (
     <main className="grid gap-4 px-3">
       <div>
         <Button asChild size="2" variant="ghost" className="gap-2">
-          <Link to="..">
+          <Link to={listId ? "/todos/$listId" : "/"} params={{ listId }}>
             <i className="fas fa-arrow-left" />
             Back
           </Link>
