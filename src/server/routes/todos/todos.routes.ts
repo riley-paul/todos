7a2bd85833134635f97a969316c43375d2735502
Riley Paul <rileypaul96@gmail.com>
2025-03-10 21:17:25 -0600

    small tweak

--- src/server/routes/todos/todos.routes.ts
+++ src/server/routes/todos/todos.routes.ts
@@ -10,7 +10,7 @@ export const list = createRoute({
   path: "/todos",
   method: "get",
   tags,
-  request: { query: z.object({ listId: z.string().nullable() }) },
+  request: { query: z.object({ listId: z.string().optional() }) },
   responses: {
     [HttpStatusCodes.OK]: jsonContent(z.array(zTodoSelect), "List of todos"),
   },
