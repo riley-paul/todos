2903e8e2075a6060cb4666ca475a5967f8d8fd3f
Riley Paul <rileypaul96@gmail.com>
2025-03-12 11:58:18 -0600

    revert config changes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,4 @@
 /// <reference types="vitest" />
 import { getViteConfig } from "astro/config";
-import { defineConfig, mergeConfig } from "vitest/config";
 
-const viteConfig = getViteConfig({});
-export default defineConfig((configEnv) =>
-  mergeConfig(
-    viteConfig(configEnv),
-    defineConfig({
-      test: {},
-    }),
-  ),
-);
+export default getViteConfig({});
