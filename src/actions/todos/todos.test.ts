a157a6fab147b1ecc4c2efefec5d27c4dd829386
Riley Paul <rileypaul96@gmail.com>
2025-03-12 12:15:19 -0600

    get first test working

--- src/actions/todos/todos.test.ts
+++ src/actions/todos/todos.test.ts
@@ -1,8 +1,47 @@
-import { expect, test } from "vitest";
+import { expect, test, describe, beforeAll, afterAll } from "vitest";
 import * as actions from "./todos.handlers";
 import mockApiContext from "../__test__/mock-api-context";
+import { execSync } from "child_process";
+import { rmSync } from "fs";
+import db from "@/db";
+import { List, Todo, User } from "@/db/schema";
 
-test("returns an array", async () => {
-  const todos = await actions.get({ listId: null }, mockApiContext("sdfadf"));
-  expect(Array.isArray(todos)).toBe(true);
+const USER_ID = crypto.randomUUID();
+const LIST_ID = crypto.randomUUID();
+
+describe("todos actions", () => {
+  beforeAll(async () => {
+    execSync("npm run db:push:test");
+
+    await db.insert(User).values({
+      id: USER_ID,
+      email: "test_user@example.com",
+      name: "Test User",
+    });
+
+    await db
+      .insert(List)
+      .values({ id: LIST_ID, name: "Test List", userId: USER_ID })
+      .returning();
+
+    const addTodo = () =>
+      db
+        .insert(Todo)
+        .values({ userId: USER_ID, listId: LIST_ID, text: "Test Todo" });
+
+    await Promise.all(Array.from({ length: 10 }, addTodo));
+  });
+
+  afterAll(() => {
+    rmSync("test.db", { force: true });
+  });
+
+  test("returns an array", async () => {
+    const todos = await actions.get(
+      { listId: LIST_ID },
+      mockApiContext(USER_ID),
+    );
+    expect(Array.isArray(todos)).toBe(true);
+    expect(todos.length).toBe(10);
+  });
 });
