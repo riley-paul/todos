a6b0b83b4f259e22a55bfcbcc649550fedbf5957
Riley Paul <rileypaul96@gmail.com>
2025-07-05 10:40:25 -0600

    get nested drawer working

--- src/components/ui/menu/menu-drawer.tsx
+++ src/components/ui/menu/menu-drawer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { MenuItem } from "./types";
-import { Button, Separator } from "@radix-ui/themes";
+import { Button, Separator, type ButtonProps } from "@radix-ui/themes";
+import { ChevronRightIcon } from "lucide-react";
+import Drawer from "../drawer";
 
 type Props = {
   menuItems: MenuItem[];
@@ -9,19 +11,40 @@ type Props = {
 const MenuDrawer: React.FC<Props> = ({ menuItems }) => {
   return menuItems.map((item, index) => {
     if (item.type === "separator") {
-      return <Separator size="4" className="mx-2 my-1 w-auto" key={`sep-${index}`} />;
+      return (
+        <Separator size="4" className="mx-2 my-1 w-auto" key={`sep-${index}`} />
+      );
+    }
+
+    const buttonProps: ButtonProps = {
+      key: item.key,
+      onClick: item.onClick,
+      disabled: item.disabled,
+      color: item.color,
+      variant: "ghost",
+      radius: "large",
+      className: "m-0 justify-start py-2 px-2 gap-0.5",
+    };
+
+    if (item.children) {
+      return (
+        <Drawer.NestedRoot>
+          <Drawer.Trigger asChild>
+            <Button {...buttonProps}>
+              {item.icon}
+              <span className="ml-2">{item.text}</span>
+              <ChevronRightIcon className="ml-auto size-4 opacity-70" />
+            </Button>
+          </Drawer.Trigger>
+          <Drawer.Content>
+            <MenuDrawer menuItems={item.children} />
+          </Drawer.Content>
+        </Drawer.NestedRoot>
+      );
     }
 
     return (
-      <Button
-        key={item.key}
-        onClick={item.onClick}
-        disabled={item.disabled}
-        color={item.color}
-        variant="ghost"
-        radius="large"
-        className="m-0 justify-start py-2 px-2 gap-0.5"
-      >
+      <Button {...buttonProps}>
         {item.icon}
         <span className="ml-2">{item.text}</span>
       </Button>
