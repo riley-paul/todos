7e4f5f0d5d0b28256e2a432d3996f80a7b5e84ed
Riley Paul <rileypaul96@gmail.com>
2025-07-05 10:31:44 -0600

    tweak styles

--- src/components/ui/menu/menu-drawer.tsx
+++ src/components/ui/menu/menu-drawer.tsx
@@ -9,7 +9,7 @@ type Props = {
 const MenuDrawer: React.FC<Props> = ({ menuItems }) => {
   return menuItems.map((item, index) => {
     if (item.type === "separator") {
-      return <Separator size="4" className="mx-3 my-1" key={`sep-${index}`} />;
+      return <Separator size="4" className="mx-2 my-1 w-auto" key={`sep-${index}`} />;
     }
 
     return (
@@ -20,7 +20,7 @@ const MenuDrawer: React.FC<Props> = ({ menuItems }) => {
         color={item.color}
         variant="ghost"
         radius="large"
-        className="m-0 justify-start py-2 px-3 gap-0.5"
+        className="m-0 justify-start py-2 px-2 gap-0.5"
       >
         {item.icon}
         <span className="ml-2">{item.text}</span>
