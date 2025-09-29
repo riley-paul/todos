c0523f52801d4824d2dddc2aa8779a1293767ba3
Riley Paul <rileypaul96@gmail.com>
2025-09-29 00:00:34 -0600

    break out todos container

---
+++ src/app/components/todo/todos-container.tsx
@@ -0,0 +1,54 @@
+import useMutations from "@/app/hooks/use-mutations";
+import { cn } from "@/lib/client/utils";
+import type { ListSelect } from "@/lib/types";
+import { Card, Tooltip, IconButton, Heading, Badge } from "@radix-ui/themes";
+import { PinIcon, MoreHorizontalIcon } from "lucide-react";
+import ListMenu from "../list/list-menu";
+import UserBubbleGroup from "../ui/user-bubble-group";
+
+const TodosContainer: React.FC<
+  React.PropsWithChildren<{ list: ListSelect }>
+> = ({ children, list }) => {
+  const { updateList } = useMutations();
+
+  const handleTogglePin = () => {
+    updateList.mutate({ id: list.id, data: { isPinned: !list.isPinned } });
+  };
+
+  return (
+    <Card size="2" className="grid gap-4">
+      <header className="flex items-center justify-between gap-4">
+        <Heading as="h2" size="4">
+          {list.name}
+        </Heading>
+        <section className="flex items-center gap-2">
+          {list.otherUsers && (
+            <UserBubbleGroup users={list.otherUsers} numAvatars={3} />
+          )}
+
+          <Badge color="gray">{list.todoCount}</Badge>
+
+          <Tooltip content={list.isPinned ? "Unpin List" : "Pin List"}>
+            <IconButton variant="ghost" color="gray" onClick={handleTogglePin}>
+              <PinIcon
+                className={cn("size-4", list.isPinned && "text-amber-9")}
+              />
+            </IconButton>
+          </Tooltip>
+
+          <ListMenu
+            list={list}
+            trigger={
+              <IconButton size="2" variant="soft" className="!m-0">
+                <MoreHorizontalIcon className="size-4" />
+              </IconButton>
+            }
+          />
+        </section>
+      </header>
+      {children}
+    </Card>
+  );
+};
+
+export default TodosContainer;
--- src/app/components/todo/todos.tsx
+++ src/app/components/todo/todos.tsx
@@ -3,26 +3,16 @@ import { cn } from "@/lib/client/utils";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
 import Todo from "./todo";
-import {
-  Badge,
-  Button,
-  Card,
-  Heading,
-  IconButton,
-  Text,
-  Tooltip,
-} from "@radix-ui/themes";
+import { Button, Card, Text } from "@radix-ui/themes";
 import type { ListSelect, SelectedList, TodoSelect } from "@/lib/types";
 import { qTodos, qUser } from "@/lib/client/queries";
-import { ChevronRightIcon, MoreHorizontalIcon, PinIcon } from "lucide-react";
+import { ChevronRightIcon } from "lucide-react";
 import DeleteCompletedTodosButton from "./footer-buttons/delete-completed-todos-button";
 import UncheckAllTodosButton from "./footer-buttons/uncheck-all-todos-button";
 
-import emptyTodoImg from "@/assets/undraw_no-data_ig65.svg";
+import emptyTodoImg from "@/assets/undraw_empty_4zx0.svg";
 import Illustration from "../illustration";
-import ListMenu from "../list/list-menu";
-import UserBubbleGroup from "../ui/user-bubble-group";
-import useMutations from "@/app/hooks/use-mutations";
+import TodosContainer from "./todos-container";
 
 const CompletedTodosActions: React.FC<{ listId: SelectedList }> = ({
   listId,
@@ -67,51 +57,6 @@ const CompletedTodosGroup: React.FC<{
       {showCompleted && (
         <div className="grid gap-1">{completedTodos.map(produceTodo)}</div>
       )}
-    </Card>
-  );
-};
-
-const TodosContainer: React.FC<
-  React.PropsWithChildren<{ list: ListSelect }>
-> = ({ children, list }) => {
-  const { updateList } = useMutations();
-
-  const handleTogglePin = () => {
-    updateList.mutate({ id: list.id, data: { isPinned: !list.isPinned } });
-  };
-
-  return (
-    <Card size="2" className="grid gap-4">
-      <header className="flex items-center justify-between gap-4">
-        <Heading as="h2" size="4">
-          {list.name}
-        </Heading>
-        <section className="flex items-center gap-2">
-          {list.otherUsers && (
-            <UserBubbleGroup users={list.otherUsers} numAvatars={3} />
-          )}
-
-          <Badge color="gray">{list.todoCount}</Badge>
-
-          <Tooltip content={list.isPinned ? "Unpin List" : "Pin List"}>
-            <IconButton variant="ghost" color="gray" onClick={handleTogglePin}>
-              <PinIcon
-                className={cn("size-4", list.isPinned && "text-amber-9")}
-              />
-            </IconButton>
-          </Tooltip>
-
-          <ListMenu
-            list={list}
-            trigger={
-              <IconButton size="2" variant="soft" className="!m-0">
-                <MoreHorizontalIcon className="size-4" />
-              </IconButton>
-            }
-          />
-        </section>
-      </header>
-      {children}
     </Card>
   );
 };
