df16a2d0b891a03b03379d58267414e22e39371f
Riley Paul <rileypaul96@gmail.com>
2025-10-27 07:45:16 -0600

    properly go to first list

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,17 +1,22 @@
-import { qLists } from "@/app/lib/queries";
+import { qLists, qUser } from "@/app/lib/queries";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import NoListsScreen from "../components/screens/no-lists";
+import { sortByOrder } from "../lib/utils";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
   loader: async ({ context: { queryClient } }) => {
     const lists = await queryClient.ensureQueryData(qLists);
+
     if (lists.length >= 1) {
+      const currentUser = await queryClient.ensureQueryData(qUser);
+      const [firstList] = sortByOrder(lists, currentUser.settingListOrder);
       throw redirect({
         to: "/todos/$listId",
-        params: { listId: lists[0].id },
+        params: { listId: firstList.id },
       });
     }
+
     return { lists };
   },
 });
