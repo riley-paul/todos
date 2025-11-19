3d78556cff11ba5fdfce29e98b4e322f4e108eff
Riley Paul <rileypaul96@gmail.com>
2025-11-18 20:38:36 -0700

    add blur to header

---
+++ src/app/components/app-header.tsx
@@ -0,0 +1,38 @@
+import { Link } from "@tanstack/react-router";
+import { CircleCheckBigIcon } from "lucide-react";
+import { Heading, Separator } from "@radix-ui/themes";
+import UserMenu from "../components/user-menu";
+import ConnectionState from "../components/connection-state";
+import AppSearch from "../components/app-search";
+
+import React from "react";
+import type { UserSelect } from "@/lib/types";
+
+type Props = {
+  currentUser: UserSelect;
+};
+
+const AppHeader: React.FC<Props> = ({ currentUser }) => {
+  return (
+    <header className="backdrop-blue-md sticky top-0 z-10 flex h-18 flex-col justify-center bg-[color-mix(in_lch,var(--background)_70%,transparent)] backdrop-blur-md">
+      <div className="container2 flex flex-1 items-center justify-between">
+        <Link to="/" className="flex items-center gap-2">
+          <CircleCheckBigIcon className="text-accent-10 size-6" />
+          <Heading size="4">Todos</Heading>
+        </Link>
+        <section className="flex items-center gap-3">
+          <ConnectionState />
+          <AppSearch />
+          <UserMenu user={currentUser} />
+        </section>
+      </div>
+      <div className="container2">
+        <div className="-mx-3">
+          <Separator size="4" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default AppHeader;
--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,19 +1,11 @@
 import { qLists, qUser } from "@/app/lib/queries";
 import { type QueryClient } from "@tanstack/react-query";
-import {
-  createRootRouteWithContext,
-  Link,
-  Outlet,
-} from "@tanstack/react-router";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import React from "react";
-import { CircleCheckBigIcon } from "lucide-react";
-import { Heading, Separator } from "@radix-ui/themes";
-import UserMenu from "../components/user-menu";
 import ListChips from "../components/list/list-chips";
-import ConnectionState from "../components/connection-state";
-import AppSearch from "../components/app-search";
 import type { UserSelect } from "@/lib/types";
+import AppHeader from "../components/app-header";
 
 type RouterContext = {
   queryClient: QueryClient;
@@ -33,24 +25,7 @@ function Component() {
   const { currentUser } = Route.useRouteContext();
   return (
     <React.Fragment>
-      <header className="sticky top-0 z-10 flex h-18 flex-col justify-center bg-[var(--background)]">
-        <div className="container2 flex flex-1 items-center justify-between">
-          <Link to="/" className="flex items-center gap-2">
-            <CircleCheckBigIcon className="text-accent-10 size-6" />
-            <Heading size="4">Todos</Heading>
-          </Link>
-          <section className="flex items-center gap-3">
-            <ConnectionState />
-            <AppSearch />
-            <UserMenu user={currentUser} />
-          </section>
-        </div>
-        <div className="container2">
-          <div className="-mx-3">
-            <Separator size="4" />
-          </div>
-        </div>
-      </header>
+      <AppHeader currentUser={currentUser} />
       <main className="container2 grid gap-6 py-6">
         <ListChips />
         <Outlet />
