acbb223893d9e30577f25efc252374dc2dbefe4d
Riley Paul <rileypaul96@gmail.com>
2026-04-10 19:31:31 -0600

    fix tests

--- src/api/functions/todos.test.ts
+++ src/api/functions/todos.test.ts
@@ -1,12 +1,11 @@
 import { expect, test, describe, beforeAll, afterAll } from "vitest";
-import mockApiContext from "../../actions/__test__/mock-api-context";
 import { execSync } from "child_process";
 import { rmSync } from "fs";
 import { List, ListUser, Todo, User } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { deleteAllData } from "@/db/scripts/delete-all-data";
 import actionErrors from "../errors";
-import * as todoHanders from "../../actions/todos/todos.handlers";
+import * as todoFunctions from "./todos";
 import { createDb } from "@/db";
 import env from "@/envs-runtime";
 
@@ -89,33 +88,31 @@ afterAll(() => {
 
 describe("todo fetching", () => {
   test("returns all todos in a list", async () => {
-    const todos = await todoHanders.getAll(
-      { listId: LIST1_ID },
-      mockApiContext(USER1_ID),
-    );
+    const todos = await todoFunctions.getAll({
+      listId: LIST1_ID,
+      userId: USER1_ID,
+    });
     expect(Array.isArray(todos)).toBe(true);
     expect(todos.length).toBe(LIST1_LENGTH);
   });
 
   test("throws error when list does not exist", async () => {
     await expect(() =>
-      todoHanders.getAll({ listId: "nonexistent" }, mockApiContext(USER1_ID)),
+      todoFunctions.getAll({ listId: "nonexistent", userId: USER1_ID }),
     ).rejects.toThrow(actionErrors.NOT_FOUND);
   });
 });
 
 describe("todo creation", () => {
   test("creates a todo", async () => {
-    const result = await todoHanders.create(
-      {
-        data: {
-          listId: LIST1_ID,
-          text: "New Todo",
-          isCompleted: false,
-        },
+    const result = await todoFunctions.create({
+      userId: USER1_ID,
+      data: {
+        listId: LIST1_ID,
+        text: "New Todo",
+        isCompleted: false,
       },
-      mockApiContext(USER1_ID),
-    );
+    });
 
     const [todo] = await db.select().from(Todo).where(eq(Todo.id, result.id));
     expect(todo.text).toBe("New Todo");
@@ -123,16 +120,14 @@ describe("todo creation", () => {
 
   test("creates a todo in shared list", async () => {
     await expect(
-      todoHanders.create(
-        {
-          data: {
-            listId: LIST3_ID,
-            text: "New Todo",
-            isCompleted: false,
-          },
+      todoFunctions.create({
+        userId: USER1_ID,
+        data: {
+          listId: LIST3_ID,
+          text: "New Todo",
+          isCompleted: false,
         },
-        mockApiContext(USER1_ID),
-      ),
+      }),
     ).resolves.not.toThrow();
   });
 
@@ -143,31 +138,27 @@ describe("todo creation", () => {
       .where(eq(ListUser.id, LIST_USER_ID));
 
     await expect(() =>
-      todoHanders.create(
-        {
-          data: {
-            listId: LIST3_ID,
-            text: "New Todo",
-            isCompleted: false,
-          },
+      todoFunctions.create({
+        userId: USER1_ID,
+        data: {
+          listId: LIST3_ID,
+          text: "New Todo",
+          isCompleted: false,
         },
-        mockApiContext(USER1_ID),
-      ),
+      }),
     ).rejects.toThrow(actionErrors.NO_PERMISSION);
   });
 
   test("throws error when list does not exist", async () => {
     await expect(() =>
-      todoHanders.create(
-        {
-          data: {
-            listId: "nonexistent",
-            text: "New Todo",
-            isCompleted: false,
-          },
+      todoFunctions.create({
+        userId: USER1_ID,
+        data: {
+          listId: "nonexistent",
+          text: "New Todo",
+          isCompleted: false,
         },
-        mockApiContext(USER1_ID),
-      ),
+      }),
     ).rejects.toThrow(actionErrors.NOT_FOUND);
   });
 
@@ -178,16 +169,14 @@ describe("todo creation", () => {
       .where(eq(ListUser.id, LIST_USER_ID));
 
     await expect(() =>
-      todoHanders.create(
-        {
-          data: {
-            listId: LIST3_ID,
-            text: "New Todo",
-            isCompleted: false,
-          },
+      todoFunctions.create({
+        userId: USER1_ID,
+        data: {
+          listId: LIST3_ID,
+          text: "New Todo",
+          isCompleted: false,
         },
-        mockApiContext(USER1_ID),
-      ),
+      }),
     ).rejects.toThrow(actionErrors.NO_PERMISSION);
   });
 });
