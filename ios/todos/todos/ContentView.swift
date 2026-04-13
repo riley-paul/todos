c3ef6965ddb712990dbaddad5bea3d1e807a19a4
Riley Paul <rileypaul96@gmail.com>
2026-04-13 13:40:50 -0600

    add login with google button

--- ios/todos/todos/ContentView.swift
+++ ios/todos/todos/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(
+                action: {},
+                label: { Text("Login with Google") },
+            ).buttonStyle(.glass).buttonSizing(.fitted)
+
         }
         .padding()
     }
