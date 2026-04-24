aa1f443cd720b618bf9e13f3935cb73559e759f7
Riley Paul <rileypaul96@gmail.com>
2026-04-24 13:41:29 -0600

    build out schema

--- src/pages/graphql.ts
+++ src/pages/graphql.ts
@@ -1,12 +1,54 @@
 import { createYoga } from "graphql-yoga";
 import type { APIRoute } from "astro";
 import { createDb } from "@/db";
-import { createSchema } from "@/lib/schema";
 import { env } from "cloudflare:workers";
+import SchemaBuilder from "@pothos/core";
+import DrizzlePlugin from "@pothos/plugin-drizzle";
+import { getTableConfig } from "drizzle-orm/sqlite-core";
+import relations from "@/db/relations";
+
+type DrizzleRelations = typeof relations;
+
+export interface PothosTypes {
+  DrizzleRelations: DrizzleRelations;
+  Context: { userId: string };
+}
+const db = createDb(env);
+
+const builder = new SchemaBuilder<PothosTypes>({
+  plugins: [DrizzlePlugin],
+  drizzle: {
+    client: db, // or (ctx) => db if you want to create a request specific client
+    getTableConfig,
+    relations,
+  },
+});
+
+const UserRef = builder.drizzleObject("User", {});
+
+const TodoRef = builder.drizzleObject("Todo", {});
+
+const ListRef = builder.drizzleObject("List", {});
+
+builder.queryType({
+  fields: (t) => ({
+    lists: t.field({
+      type: [ListRef],
+      resolve: async () => {
+        return db.query.List.findMany();
+      },
+    }),
+  }),
+});
 
 export const ALL: APIRoute = async (ctx) => {
-  const db = createDb(env);
-  const schema = createSchema(db);
-  const yoga = createYoga({ schema, fetchAPI: { Response } });
+  const user = ctx.locals.user;
+  if (!user) return new Response("Unauthorized", { status: 401 });
+
+  const yoga = createYoga({
+    schema: builder.toSchema(),
+    context: { userId: user.id },
+    fetchAPI: { Response },
+  });
   return yoga(ctx.request);
 };
