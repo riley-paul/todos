dc4a571d3166ca180f16e1a0db04498273e4d0c4
Riley Paul <rileypaul96@gmail.com>
2026-05-04 20:53:38 -0600

    reorganize component

--- src/app/routes/settings.tsx
+++ src/app/routes/settings.tsx
@@ -35,10 +35,7 @@ const Setting: React.FC<SettingProps> = ({ label, children }) => (
   </Text>
 );
 
-function RouteComponent() {
-  const { data: settings } = useSuspenseQuery(qUser());
-
-  const [theme, setTheme] = useAtom(themeAtom);
+const DeleteAccountSetting: React.FC = () => {
   const [, dispatchAlert] = useAtom(alertSystemAtom);
 
   const deleteAccountMutation = useMutation({
@@ -48,8 +45,6 @@ function RouteComponent() {
       window.location.reload();
     },
   });
-
-  const { updateUserSettings } = useMutations();
 
   const handleDeleteAccount = () => {
     dispatchAlert({
@@ -65,41 +60,71 @@ function RouteComponent() {
   };
 
   return (
+    <Setting label="Delete account">
+      <Button
+        size="1"
+        color="red"
+        variant="surface"
+        onClick={handleDeleteAccount}
+      >
+        <Trash2Icon className="size-4" />
+        Delete
+      </Button>
+    </Setting>
+  );
+};
+
+const ThemeSetting: React.FC = () => {
+  const [theme, setTheme] = useAtom(themeAtom);
+  return (
+    <Setting label="Dark mode">
+      <SegmentedControl.Root
+        size="1"
+        value={theme}
+        onValueChange={(value) => setTheme(value as any)}
+      >
+        <SegmentedControl.Item value="system">System</SegmentedControl.Item>
+        <SegmentedControl.Item value="dark">Dark</SegmentedControl.Item>
+        <SegmentedControl.Item value="light">Light</SegmentedControl.Item>
+      </SegmentedControl.Root>
+    </Setting>
+  );
+};
+
+const GroupCompletedSetting: React.FC = () => {
+  const { data: settings } = useSuspenseQuery(qUser());
+  const { updateUserSettings } = useMutations();
+
+  return (
+    <Setting label="Group completed todos">
+      <Switch
+        checked={settings.settingGroupCompleted}
+        onCheckedChange={(value) => {
+          updateUserSettings.mutate({
+            settingGroupCompleted: value,
+          });
+        }}
+      />
+    </Setting>
+  );
+};
+
+function RouteComponent() {
+  return (
     <React.Fragment>
       <Heading size="7">Settings</Heading>
       <article className="grid gap-4">
         <Card className="grid gap-5">
-          <Setting label="Group completed todos">
-            <Switch
-              checked={settings.settingGroupCompleted}
-              onCheckedChange={(value) => {
-                updateUserSettings.mutate({
-                  settingGroupCompleted: value,
-                });
-              }}
-            />
-          </Setting>
-          <Setting label="Dark mode">
-            <SegmentedControl.Root
-              size="1"
-              value={theme}
-              onValueChange={(value) => setTheme(value as any)}
-            >
-              <SegmentedControl.Item value="system">
-                System
-              </SegmentedControl.Item>
-              <SegmentedControl.Item value="dark">Dark</SegmentedControl.Item>
-              <SegmentedControl.Item value="light">Light</SegmentedControl.Item>
-            </SegmentedControl.Root>
+          <GroupCompletedSetting />
+          <ThemeSetting />
+        </Card>
+        <Card>
+          <Setting label="Enable notifications">
+            <Button size="1">Enable</Button>
           </Setting>
         </Card>
         <Card>
-          <Setting label="Delete account">
-            <Button size="1" color="red" onClick={handleDeleteAccount}>
-              <Trash2Icon className="size-4" />
-              Delete
-            </Button>
-          </Setting>
+          <DeleteAccountSetting />
         </Card>
       </article>
     </React.Fragment>
